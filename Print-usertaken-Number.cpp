bf999ab30316fd62fee01fe397ace2581e108e48
sumit Kumar <sumitkumar4098765@gmail.com>
2022-12-26 13:18:09 +0530

    personal repository

---
+++ Print-usertaken-Number.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int No;
+    cout<<"Enter the Number:";//'<<' is called Insertion Operator
+    cin>>No;//'>>' is called Extersion Operator
+    cout<<"Number is:"<<No<<"\n";
+}
